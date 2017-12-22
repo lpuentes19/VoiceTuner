21c783b3d67878b49a48103d78f901dad4245883
lpuentes19 <luey.puentes@gmail.com>
2017-12-22 10:47:29 -0700

    Imported AVFoundation and added some logic to the record and stopRecording buttons

--- VoiceTuner/RecordingVoiceViewController.swift
+++ VoiceTuner/RecordingVoiceViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordingVoiceViewController: UIViewController {
 
+    var audioRecorder: AVAudioRecorder!
+    
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
@@ -21,16 +24,31 @@ class RecordingVoiceViewController: UIViewController {
     }
     
     @IBAction func recordButtonTapped(_ sender: Any) {
-        recordingLabel.text = "Recording in progress."
-        recordingLabel.text = "Recording in progress.."
         recordingLabel.text = "Recording in progress..."
         recordButton.isEnabled = false
         stopRecordingButton.isEnabled = true
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     
     @IBAction func stopRecordingButtonTapped(_ sender: Any) {
         recordingLabel.text = "Tap to Record"
         stopRecordingButton.isEnabled = false
         recordButton.isEnabled = true
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
 }
