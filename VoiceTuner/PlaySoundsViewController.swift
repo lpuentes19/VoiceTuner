ed6960e28f7229092d1f547283d769b316a40dda
lpuentes19 <luey.puentes@gmail.com>
2017-12-22 13:43:38 -0700

    Added IBOutlets and IBAction to all the buttons I added on the PlaySoundsVC. Also, added the buttons in a stack view within a vertical stack view, AutoLayout is complete

--- VoiceTuner/PlaySoundsViewController.swift
+++ VoiceTuner/PlaySoundsViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
 
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     var recordedAudioURL: URL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
+    @IBAction func playButtonTapped(_ sender: Any) {
+        print("Hi")
+    }
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        print("Stop")
+    }
 }
